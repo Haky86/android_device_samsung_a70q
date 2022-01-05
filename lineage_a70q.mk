1fb9c954c10aa78ba2df7206d81035a72ff116a4
Haky86 <hakamelassouad@gmail.com>
2022-01-05 14:30:52 +0100

    a70q: Drop aosp_arm64_ab makefile
    * aosp_arm64_ab is no longer needed, since we load the ramdisk via kernel.
    
    Change-Id: I52c9b03c94edae069e71d7c5283a406be084ae6c

--- lineage_a70q.mk
+++ lineage_a70q.mk
@@ -15,7 +15,6 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_arm64_ab.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
